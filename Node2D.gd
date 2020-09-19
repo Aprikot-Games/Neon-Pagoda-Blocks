52755d0f889289f4900c7d10c6af8440c6dbf998
Gecko05 <jacacent@gmail.com>
2020-09-18 20:54:59 -0500

    pendulum sprite added

---
+++ Node2D.gd
@@ -0,0 +1,45 @@
+extends Position2D
+#class_name Pendulum
+
+var pivot_point:Vector2
+export (Vector2) var end_position: = Vector2()
+var arm_length:float
+var angle
+var offset = Vector2(0, 0)
+
+export (float) var gravity = 0.4 * 60
+#export (float) var damping = 0.995 
+
+var angular_velocity = 0.0
+var angular_acceleration = 0.0
+
+func _ready()->void:
+	set_start_position(global_position, end_position)
+
+func set_start_position(start_pos:Vector2, end_pos:Vector2):
+	pivot_point = start_pos
+	end_position = end_pos
+	arm_length = Vector2.ZERO.distance_to(end_position-pivot_point)
+	angle = Vector2.ZERO.angle_to(end_position-pivot_point) - deg2rad(-90)
+	angular_velocity = 0.0
+	angular_acceleration = 0.0
+
+func process_velocity(delta:float)->void:
+	angular_acceleration = ((-gravity*delta) / arm_length) *sin(angle)
+	angular_velocity += angular_acceleration
+	#angular_velocity *= damping
+	angle += angular_velocity
+	
+	end_position = pivot_point + Vector2(arm_length*sin(angle), arm_length*cos(angle))
+
+func add_angular_velocity(force:float)->void:
+	angular_velocity += force
+
+func _physics_process(delta)->void:
+	process_velocity(delta)
+	update()												#draw
+
+func _draw()->void:
+	#draw_line(Vector2.ZERO, end_position - pivot_point, Color.white, 1.0, false)
+	#draw_circle(end_position - pivot_point + offset, 3.0, Color.red)
+	position = end_position - pivot_point + offset
--- TowerBlock.gd
+++ TowerBlock.gd
@@ -7,12 +7,35 @@ enum TYPE {BASE,SLOW,FIXER,WIDE}
 export var wide = false
 export var next = 0
 export var prev = 0
+var pos = Vector2(10, PI/2)
+export var hang = true
+var c = 0
+var debug = true
+
+func dbg_print(x):
+	if debug == true:
+		print(x)
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
-func _process(delta);
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+# Oscillation for the block, speed should vary depending on the increment
+func swing():
+	var r = pos[0]
+	var th = pos[1]
+	var c_n = c / 60
+	th = PI/2 + ((PI/4)*(sin(c_n)))
+	var cart_pos = polar2cartesian(r, th)
+	self.position.x = cart_pos[0]
+	self.position.y = cart_pos[1]
+	c += 1
+	if c > 60:
+		c = 0
+
+func _process(delta):
+	if delta >= 0.01667:
+		if hang == true:
+			swing()
+
+func on_drop():
+	dbg_print("Drop!")
