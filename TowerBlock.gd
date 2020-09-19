c608ec42eac62e17d76a7b5365426d3ac40a9b31
Gecko05 <jacacent@gmail.com>
2020-09-18 19:53:55 -0500

    dada

---
+++ TowerBlock.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+enum TYPE {BASE,SLOW,FIXER,WIDE}
+export var wide = false
+export var next = 0
+export var prev = 0
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta);
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
