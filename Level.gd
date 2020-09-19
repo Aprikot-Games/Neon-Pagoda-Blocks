a695faa7fc24bcc33c0d1b93a405d4a63254b67a
Gecko05 <jacacent@gmail.com>
2020-09-19 17:42:58 -0500

    Added functionality to create and drop a new tower block on the same position as the pendulum

--- Level.gd
+++ Level.gd
@@ -1,11 +1,23 @@
 extends Node2D
 
+export (PackedScene) var TowerRigidBody
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 var stack = []
+var currentBlock
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+
+func _on_drop_block():
+	# Create a block to drop onto the tower
+	var new_block = TowerRigidBody.instance()
+	add_child(new_block)
+	new_block.position = $TowerBlock.position
+	new_block.connect("body_entered", self, "_on_TowerBlock_body_entered")
+
+func _on_block_ready():
+	print("End")
--- TowerRigidBody.gd
+++ TowerRigidBody.gd
@@ -1,37 +1,27 @@
 extends RigidBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+signal block_ready
 var state = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if state == 2:
+	if state == 1:
 		if angular_velocity < 0.1 and linear_velocity < Vector2(0.1,0.1):
 			print("Stabilized")
-			state = 3
+			mode = RigidBody2D.MODE_STATIC
 		else:
 			print("Waiting")
-
-
-func _on_Button_pressed():
-	print("Pressed")
-	var joint = get_node("../PinJoint2D2")
-	var placeholder = get_node("../Placeholder")
-	joint.node_b = placeholder.get_path()
-	#joint.queue_free()
+			self.queue_free()
+		state = 2
+		emit_signal("block_ready")
 
 func _on_TowerBlock_body_entered(body):
 	$Timer.start()
 
-
 func _on_Timer_timeout():
-	state = 2
+	state = 1
 	print("Finished")
