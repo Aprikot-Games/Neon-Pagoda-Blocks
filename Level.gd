62204c3c445f6a57b9060fde27f9b334f92cab81
Gecko05 <jacacent@gmail.com>
2020-09-19 19:35:07 -0500

    Added animations for lowering the landscape and tower

--- Level.gd
+++ Level.gd
@@ -5,19 +5,56 @@ export (PackedScene) var TowerRigidBody
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var stack = []
-var currentBlock
+var tower = []
+var base_block
+var current_block
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$TowerBlock.set_collision_layer_bit(1, false)
+	$TowerBlock.set_collision_mask_bit(0, false)
+	tower.append($TowerBase)
 
 func _on_drop_block():
 	# Create a block to drop onto the tower
 	var new_block = TowerRigidBody.instance()
 	add_child(new_block)
 	new_block.position = $TowerBlock.position
-	new_block.connect("body_entered", self, "_on_TowerBlock_body_entered")
+	new_block.connect("block_ready", self, "_on_block_ready")
+	$TowerBlock.hide()
+	current_block = new_block
 
-func _on_block_ready():
-	print("End")
+func shift_landscape():
+	var tween = get_node("Back1/Tween")
+	tween.interpolate_property($Back1, "position",
+			Vector2($Back1.position.x, $Back1.position.y), 
+			Vector2($Back1.position.x, $Back1.position.y + 85), 1,
+			Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween.start()
+	tween = get_node("Back2/Tween")
+	tween.interpolate_property($Back2, "position",
+			Vector2($Back2.position.x, $Back2.position.y), 
+			Vector2($Back2.position.x, $Back2.position.y + 42.5), 1,
+			Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween.start()
+
+func shift_block(block):
+	var tween = block.get_node("Tween")
+	tween.interpolate_property(block, "position",
+			Vector2(block.position.x, block.position.y), 
+			Vector2(block.position.x, block.position.y + 85), 1,
+			Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween.start()
+
+func _on_block_ready(res):
+	print("End of drop")
+	$TowerBlock.show()
+	if res == true:
+		tower.append(current_block)
+		# Shift the whole tower down
+		for block in tower:
+			if block == null:
+				tower.remove(0)
+			else:
+				shift_block(block)
+		shift_landscape()
--- TowerRigidBody.gd
+++ TowerRigidBody.gd
@@ -1,6 +1,6 @@
 extends RigidBody2D
 
-signal block_ready
+signal block_ready(res)
 var state = 0
 
 # Called when the node enters the scene tree for the first time.
@@ -10,18 +10,24 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if state == 1:
-		if angular_velocity < 0.1 and linear_velocity < Vector2(0.1,0.1):
+		if abs(angular_velocity) < 0.1:
 			print("Stabilized")
 			mode = RigidBody2D.MODE_STATIC
+			self.set_collision_layer_bit(1, false)
+			self.set_collision_layer_bit(0, true)
+			state = 2
+			emit_signal("block_ready", true)
 		else:
 			print("Waiting")
-			self.queue_free()
-		state = 2
-		emit_signal("block_ready")
 
-func _on_TowerBlock_body_entered(body):
+func _on_body_entered(body):
 	$Timer.start()
 
 func _on_Timer_timeout():
 	state = 1
 	print("Finished")
+
+func _on_screen_exited():
+	print("Erased!")
+	emit_signal("block_ready", false)
+	queue_free()
